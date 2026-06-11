f85f1394e89770c8bf574babada1c9c40a82e1c7
Pal Heights <haneet@palheights.com>
2026-06-11 14:36:58 +0530

    Update Home.tsx

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,20 +1,21 @@
 import Hero from "./Sections/VideoHero";
 import SectionOne from "./Sections/Properties";
-// import SectionTwo from "./Sections/ExperiencesSlider";
-import SectionThree from "./Sections/Products";
-import SectionFour from "./Sections/Blogs";
-import SectionFive from "../sharedSections/VideoTestimonials";
+import SectionTwo from "./Sections/ExperiencesSlider";
+import SectionThree from "./Sections/Restaurants";
+// import SectionFour from "./Sections/Products";
+import SectionFive from "./Sections/Blogs";
+import SectionSix from "../sharedSections/VideoTestimonials";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <SectionOne />
-      {/* <SectionTwo /> */}
+      <SectionTwo />
       <SectionThree />
-      <SectionFour />
+      {/* <SectionFour /> */}
       <SectionFive />
+      <SectionSix />
     </>
   );
 }
-
