6e41e2a73100807f36e95405f1758f3fb637e3a8
draftworks-repo <draftworks000@gmail.com>
2026-06-18 13:54:54 +0530

    next config changes for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-df2be1f0ac924e4f81cce390b6cc6cee.r2.dev",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
