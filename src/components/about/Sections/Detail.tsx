59a19eeda08ba575e400bf72c5b6d32e7ff86a87
draftworks-repo <draftworks000@gmail.com>
2026-06-18 15:21:50 +0530

    avout section fixed

--- src/components/about/Sections/Detail.tsx
+++ src/components/about/Sections/Detail.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styles from "./Detail.module.css";
